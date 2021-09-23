675200c62ad8fa2abcd17caebf1d5ee55b19b0ea
Evan <evan.mactaggart@gmail.com>
2021-09-23 16:02:49 -0600

    serato tests

--- tests/test_serato.py
+++ tests/test_serato.py
@@ -1,88 +1,67 @@
 import os
+from tagger.utils import drop_keys
+from tagger.core import EasyFileUtils
 from tagger import serato, serato_tags_scripts_database_v2
 from tests.test_cmd import resource
 from unittest.case import TestCase
 
-serato_directory = resource("_Serato_")
-
-# serato_full = "/home/evan/Development/tagger/tests/resources/TaggerFiles/_Serato_full"
-# serato_true = "/home/evan/Development/tagger/tests/resources/TaggerFiles/_Serato_"
-# serato_local = "/home/evan/Development/tagger/tests/resources/TaggerFiles/_Serato_local"
-# serato_local_big = (
-#     "/home/evan/Development/tagger/tests/resources/TaggerFiles/_Serato_local_big"
-# )
-# serato_remote_big = (
-#     "/home/evan/Development/tagger/tests/resources/TaggerFiles/_Serato_remote_big"
-# )
-
 
 serato_directory = resource("_Serato_")
 
 
-def wow():
-    tags = serato.tags_from_library(serato_directory)
-
-    assert drop_keys(tags, []) == [
-        {
-            "playlists": [],
-            "crates": [],
-            "filename": "test.mp3",
-            "title": "title",
-            "artist": "artist",
-            "album": "album",
-            "genre": "#g2 #genre",
-            "comment": "@comment",
-            "_filename": "/tagger/tests/resources/test.mp3",
-        },
-        {
-            "playlists": [],
-            "crates": [],
-            "filename": "test.m4a",
-            "title": "title",
-            "artist": "artist",
-            "album": "album",
-            "genre": "#g2 #genre",
-            "comment": "@comment",
-            "_filename": "/tagger/tests/resources/test.m4a",
-        },
-        {
-            "playlists": [],
-            "crates": [],
-            "filename": "test.flac",
-            "title": "title",
-            "artist": "artist",
-            "album": "album",
-            "genre": "#g2 #genre",
-            "comment": "@comment",
-            "_filename": "/tagger/tests/resources/test.flac",
-        },
-        {
-            "playlists": [],
-            "crates": [],
-            "filename": "error.wav",
-            "title": "title",
-            "artist": "artist",
-            "album": "album",
-            "genre": "#genre",
-            "comment": "@comment",
-            "_filename": "/tagger/tests/resources/error.wav",
-        },
-        {
-            "playlists": [],
-            "crates": [],
-            "filename": "error.aiff",
-            "title": "error",
-            "_filename": "/tagger/tests/resources/error.aiff",
-        },
-        {
-            "playlists": [],
-            "crates": [],
-            "filename": "flavour-town.mp3",
-            "title": "title",
-            "artist": "artist",
-            "album": "album",
-            "genre": "#g2 #genre",
-            "comment": "@comment",
-            "_filename": "/tagger/tests/resources/flavour-town.mp3",
-        },
-    ]
+class SeratoTests(TestCase):
+    def test_serato_get_library(self):
+        tags = serato.tags_from_library(serato_directory)
+        assert drop_keys(tags, []) == [
+            {
+                "playlists": [],
+                "crates": ["!abc", "#def", "hig"],
+                "filename": "test.mp3",
+                "title": "title",
+                "artist": "artist",
+                "album": "album",
+                "genre": "#g2 #genre",
+                "comment": "@comment",
+                "_filename": "/tagger/tests/resources/test.mp3",
+            },
+            {
+                "playlists": [],
+                "crates": ["!abc", "#def", "hig"],
+                "filename": "test.m4a",
+                "title": "title",
+                "artist": "artist",
+                "album": "album",
+                "genre": "#g2 #genre",
+                "comment": "@comment",
+                "_filename": "/tagger/tests/resources/test.m4a",
+            },
+            {
+                "playlists": [],
+                "crates": ["!abc", "#def", "hig"],
+                "filename": "test.flac",
+                "title": "title",
+                "artist": "artist",
+                "album": "album",
+                "genre": "#g2 #genre",
+                "comment": "@comment",
+                "_filename": "/tagger/tests/resources/test.flac",
+            },
+            {
+                "playlists": [],
+                "crates": ["!abc", "#def", "hig", "#dnb"],
+                "filename": "error.wav",
+                "title": "title",
+                "artist": "artist",
+                "album": "album",
+                "genre": "#genre",
+                "comment": "@comment",
+                "_filename": "/tagger/tests/resources/error.wav",
+            },
+            {
+                "playlists": [],
+                "crates": [],
+                "filename": "error.aiff",
+                "title": "error",
+                "_filename": "/tagger/tests/resources/error.aiff",
+            },
+        ]
